d433f28c830e8a4f92a99e3f3293992d5d9c4753
Borsos Laszlo <lborsos@gmail.com>
2025-01-07 22:32:36 +0200

    Bevewto

--- test.hs
+++ test.hs
@@ -3,3 +3,23 @@ module Test where
 one :: Int
 one = 10
 
+
+add :: Int -> Int -> Int
+add x y = x + y
+
+
+greater :: Int -> Int -> Bool -- True / False
+greater a b = a > b
+--greater a b = (>) a b
+
+even' :: Int -> Bool
+even' n = mod n 2 == 0
+
+odd' :: Int -> Bool
+odd' n = mod n 2 /= 0
+
+odd'' :: Int -> Bool
+odd'' n = not (even' n)
+
+trside :: Int -> Int -> Int -> Bool
+trside a b c = a + b > c && a + c > b && b + c > a
